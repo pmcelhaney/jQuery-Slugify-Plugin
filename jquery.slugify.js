4b9a176d5c99008e55f032ef274dfbef16ea9a17
Patrick McElhaney <pmcelhaney@gmail.com>
2010-09-22 21:12:32 -0400

    Cleaned up spaces in source code.

--- jquery.slugify.js
+++ jquery.slugify.js
@@ -1,48 +1,48 @@
 (function ($) {
-       
-  $.fn.slugify = function (source, options) {
-    var $target = this;            
-    var $source = $(source);            
-  
-  	var settings = $.extend({
-         slugFunc: (function (val, originalFunc) { return originalFunc(val); })
-      }, options);
-	             
 
-    var convertToSlug = function(val) {
-      return settings.slugFunc(val, 
-        (function(v) {
-          if (!v) return '';
-        	return v.replace(/'/g, '').replace(/\s*&\s*/g, ' and ').replace(/[^A-Za-z0-9]+/g, '-').replace(/^-|-$/g, '').toLowerCase();
-        })  
-      );
-    }
-	
-    var setLock = function () {
-      if($target.val() != null && $target.val() != '') {
-        $target.addClass('slugify-locked');
-      } else {
-        $target.removeClass('slugify-locked');
-      }
-    }   
+	$.fn.slugify = function (source, options) {
+		var $target = this;
+		var $source = $(source);
 
-    var updateSlug = function () {
-      var slug = convertToSlug($(this).val());
-  	  $target.filter(':not(.slugify-locked)').val(slug).text(slug);		
-  	}                   
-            
+		var settings = $.extend({
+			slugFunc: (function (val, originalFunc) { return originalFunc(val); })
+		}, options);
 
-    $source.keyup( updateSlug ).change( updateSlug ); 
-  
-  	$target.change(function () {       
-      var slug = convertToSlug($(this).val());
-      $target.val(slug).text(slug);
-      setLock();
-    });   
-        
-    setLock();         
-    
-    return this; 
-  };
+
+		var convertToSlug = function(val) {
+			return settings.slugFunc(val, 
+				(function(v) {
+					if (!v) return '';
+					return v.replace(/'/g, '').replace(/\s*&\s*/g, ' and ').replace(/[^A-Za-z0-9]+/g, '-').replace(/^-|-$/g, '').toLowerCase();
+				})  
+			);
+		}
+
+		var setLock = function () {
+			if($target.val() != null && $target.val() != '') {
+				$target.addClass('slugify-locked');
+			} else {
+				$target.removeClass('slugify-locked');
+			}
+		}
+
+		var updateSlug = function () {
+			var slug = convertToSlug($(this).val());
+			$target.filter(':not(.slugify-locked)').val(slug).text(slug);		
+		}
+
+
+		$source.keyup( updateSlug ).change( updateSlug ); 
+
+		$target.change(function () {       
+			var slug = convertToSlug($(this).val());
+			$target.val(slug).text(slug);
+			setLock();
+		});   
+
+		setLock();         
+
+		return this; 
+	};
     
 })(jQuery);             
